7579818970dab90ba2c2ab5a8e6238ba60b4506f
Jack Nguyen <nguyen.jack@gmail.com>
2014-03-28 08:43:31 -0700

    AWS S3 upload integration

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,6 @@
 class IdeasController < ApplicationController
 	before_action :set_idea, only: [:show, :edit, :update, :destroy]
+	before_filter :authenticate_user!, except: [:index, :show]
 
 	def index
 		@ideas = Idea.all
